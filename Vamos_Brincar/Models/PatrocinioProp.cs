1d8bc00e0c85ef043eeb0501f777bf60ce732f44
PauloBarros-creator <62844951+PauloBarros-creator@users.noreply.github.com>
2021-04-21 17:48:06 +0100

    Revert "Revert "GetPatrocinio""
    This reverts commit 8ffb18ada3d3929b57dabb419c33aa9d4c33ba36.

---
+++ Vamos_Brincar/Models/PatrocinioProp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vamos_Brincar.Models
+{
+    public class PatrocinioProp
+    {
+        public int id_parceiro { get; set; }
+        public string nome { get; set; }
+        public string descricao { get; set; }
+
+    }
+}
